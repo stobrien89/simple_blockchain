ae7cef723370b79f1a3e932c72ae7217a6ea016f
Sean O'Brien <60306702+stobrien89@users.noreply.github.com>
2021-11-22 18:50:14 -0800

    add third block

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ t1 = "x sends 2.5 NC to y"
 t2 = "a sends 1.5 NC to z" 
 t3 = "w sends .5 NC to r" 
 t4 = "s sends 3 NC to t" 
-t5 = "e sends .75 NC to f"        
+t5 = "e sends .75 NC to f"  
+t6 = "a sends 50 NC to g"      
 
 initial_block = NeuralCoinBlock("let's pretend this is a prior transaction", [t1, t2])
 
@@ -24,3 +25,8 @@ second_block = NeuralCoinBlock(initial_block.block_hash, [t3, t4])
 
 print(second_block.block_data)
 print(second_block.block_hash)
+
+thirdeifjcctrbi_block = NeuralCoinBlock(initial_block.block_hash, [t5, t6])
+
+print(third_block.block_data)
+print(third_block.block_hash)
