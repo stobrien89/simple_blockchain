d7f0936880555b64caaaeb8835c7951fbe0b208f
Sean O'Brien <60306702+stobrien89@users.noreply.github.com>
2021-11-22 18:39:35 -0800

    instantiate block

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ class NeuralCoinBlock:
         self.block_data =  "-".join(transaction_list) + "-" + previous_block_hash
         self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest
 
-        
+t1 = "x sends 2.5 NC to y"
+t2 = "a sends 1.5 NC to z" 
+t3 = "w sends .5 NC to r" 
+t4 = "s sends 3 NC to t" 
+t5 = "e sends .75 NC to f"        
 
+initial_block = NeuralCoinBlock("let's pretend this is a prior transaction", [t1, t2])
 
 
